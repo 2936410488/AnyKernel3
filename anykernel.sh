3327e0afdb4ea68ebd7ac58a0981f77c751e9e19
2936410488 <132274351+2936410488@users.noreply.github.com>
2023-12-12 17:38:25 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -29,12 +29,30 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 ## boot shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh && attributes;
+
+
+
+ui_print " ";
+# FUSE Passthrough
+android_ver=$(file_getprop /system/build.prop ro.build.version.release);
+fuse_passthrough=$(getprop persist.sys.fuse.passthrough.enable);
+BBOX="$home/tools/busybox"
+
+if [ $android_ver -gt 11 ] && [ $fuse_passthrough != "true" ]; then
+	ui_print "Remounting /system as rw..."
+	$BBOX mount -o rw,remount /system
+	ui_print "Patching system's build prop for FUSE Passthrough..."
+	patch_prop /system/build.prop "persist.sys.fuse.passthrough.enable" "true"
+else
+	ui_print "Ignoring FUSE Passthrough installation..."
+	ui_print "Unsupported Android version or previously installed..."
+fi
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
